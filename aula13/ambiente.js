dd268908d569cb59aede32679e6c2046751b19d5
Lucas-0802 <lucasbrito775823@gmail.com>
2022-10-04 17:34:36 -0300

    Atualização

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*var c = 1 //while
+while( c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+var d = 1 // do while
+do{
+    console.log(`Passo ${d}`)
+    d++
+}while (d <= 6)*/
+console.log('Vai começar...')
+for(var a = 1; a <= 10; a++){
+    console.log(`${a}`)
+} console.log('FIM')
